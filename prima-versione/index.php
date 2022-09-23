0ca5037bf5db03f4d996d032c46a349c3017899f
spellcry <sangiothebest@hotmail.com>
2022-09-23 17:52:20 +0200

    add prima versione - seconda milestone

--- prima-versione/index.php
+++ prima-versione/index.php
@@ -5,22 +5,42 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dischi PHP</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
     <?php
     include __DIR__ . '/../db/dischi.php';
     ?>
 
-    <ul>
-        <?php
-        foreach ($dischi as $disco) {
-            ?>
-            <li>
-                <?= "{$disco['title']}<br>{$disco['author']}<br>{$disco['genre']}<br>{$disco['year']}<br>{$disco['poster']}" ?>
-            </li>
-            <?php
-        }
-        ?>
-    </ul>
+    <div id="app">
+        <header class="main-header">
+            <img src="../assets/spotify_logo.png" alt="">
+        </header>
+        <main class="main-content">
+            <div class="container">
+                <ul class="disks-list">
+                    <?php
+                    foreach ($dischi as $disco) {
+                        ?>
+                        <li class="col">
+                            <div class="card">
+                                <img src="<?= $disco['poster'] ?>" alt="" class="card__img">
+                                <div class="card__description">
+                                    <h4 class="title"><?= $disco['title'] ?></h4>
+                                    <div class="author-year">
+                                        <p class="author"><?= $disco['author'] ?></p>
+                                        <p class="year"><?= $disco['year'] ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <?php
+                    }
+                    ?>
+                </ul>
+            </div>
+        </main>
+    </div>
 </body>
 </html>
