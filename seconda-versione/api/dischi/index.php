972e377a574f04b1e0c45468ef4a4f8260cfd12d
spellcry <sangiothebest@hotmail.com>
2022-09-23 17:46:20 +0200

    add bonus

--- seconda-versione/api/dischi/index.php
+++ seconda-versione/api/dischi/index.php
@@ -1,11 +1,19 @@
 <?php
 include __DIR__ . '/../../../db/dischi.php';
 
+$genre = isset($_GET['genre']) ? $_GET['genre'] : null;
+
+if ( $genre ) {
+    $dischi = array_filter($dischi, function($disco) use ($genre) {
+        return $disco['genre'] == $genre;
+    });
+}
+
 header('Content-Type: application/json');
 
 $data = [
     'success' => true,
-    'response' => $dischi
+    'response' => array_values($dischi)
 ];
 
 echo json_encode($data);
--- seconda-versione/main.js
+++ seconda-versione/main.js
@@ -2,12 +2,26 @@ const app = new Vue({
     el: '#app',
     data: {
         disks: [],
+        genres: [],
+        genreSelected: '',
     },
     methods: {
         fetchDisks() {
-            axios.get('./api/dischi/')
+            axios.get('./api/dischi/index.php', {
+                params: {
+                    genre: this.genreSelected
+                }
+            })
                 .then(res => {
                     this.disks = res.data.response;
+                    this.disks.forEach(disk => {
+                        !this.genres.includes(disk.genre) ? this.genres.push(disk.genre) : '';
+                    });
+                    this.genres.sort((genre1, genre2) => {
+                        const gen1 = genre1.toLowerCase();
+                        const gen2 = genre2.toLowerCase();
+                        return gen1 < gen2 ? -1 : gen1 > gen2 ? 1 : 0;
+                    })
                 })
         }
     },
